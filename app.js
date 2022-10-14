d86fff76832aef8849fa13d3ba3368c8a8f82561
Fernando Maldonado <fermaldonadosolis@gmail.com>
2022-10-13 20:27:27 -0500

    conectado con mongo db

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express")
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const port = process.env.PORT || 3000
+
+//Conexión a base de datos
+const mongoose = require('mongoose');
+
+const user = "drdelarosa"
+const password = "3YF5RORu10qyBg4Q"
+const db = "veterinaria"
+const uri = `mongodb+srv://${user}:${password}@cluster0.kesyhsa.mongodb.net/?retryWrites=true&w=majority`
+
+
+mongoose.connect(uri)
+
+    .then(() => console.log("conexión exitosa"))
+    .catch(e => console.log(e))
+
 
 //motor de plantillas
 app.set("view engine", "ejs")
@@ -12,7 +29,9 @@ app.use(express.static(__dirname + "/public"))
 
 // Rutas web
 app.use("/", require("./router/rutas")) //las rutas
+app.use("/mascotas", require("./router/Mascotas"))
 
+//Ruta 404
 app.use((req, res, next) => {
     res.status(404).render("404")
 })
---
+++ models/mascota.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const mascotaSchema = new Schema({
+    nombre: String,
+    descripcion: String
+})
+
+//Crear modelo
+const Mascota = mongoose.model("Mascota", mascotaSchema, "mascotas")
+
+module.exports = Mascota
